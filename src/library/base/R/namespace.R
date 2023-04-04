daroczig/R-weblate-fork

commit 66541df93c88e5c767f53cd72d50a7aa2183caee

make `%*%` generic; mostly contributed by Tomas Kalinowski
git-svn-id: https://svn.r-project.org/R/trunk@84154 00db46b3-68df-0310-9c12-caf00c1e9a41

src/library/base/R/namespace.R
@@ -1,7 +1,7 @@
 #  File src/library/base/R/namespace.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2021 The R Core Team
+#  Copyright (C) 1995-2023 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -830,7 +830,6 @@ requireNamespace <- function (package, ..., quietly = FALSE)
     ns <- .Internal(getRegisteredNamespace(package))
     res <- TRUE
     if (is.null(ns)) {
-        if(!quietly)
             packageStartupMessage(gettextf("Loading required namespace: %s",
                                            package), domain = NA)
         value <- tryCatch(loadNamespace(package, ...), error = function(e) e)
@@ -1539,7 +1538,7 @@ registerS3method <- function(genname, class, method, envir = parent.frame()) {
 		      c(generic, class, method, NA_character_))
         setNamespaceInfo(ns, "S3methods", regs)
     }
-    groupGenerics <- c("Math", "Ops",  "Summary", "Complex")
+    groupGenerics <- c("Math", "Ops", "matrixOps", "Summary", "Complex")
     defenv <- if(genname %in% groupGenerics) .BaseNamespaceEnv
     else {
         genfun <- get(genname, envir = envir)

src/library/base/R/zzz.R
@@ -25,7 +25,7 @@ is.name <- is.symbol
 .knownS3Generics <- local({
 
     ## include the S3 group generics here
-    baseGenerics <- c("Math", "Ops", "Summary", "Complex",
+    baseGenerics <- c("Math", "Ops", "Summary", "Complex", "matrixOps",
         "as.character", "as.data.frame", "as.environment", "as.matrix", "as.vector",
         "cbind", "labels", "print", "rbind", "rep", "seq", "seq.int",
         "plot", "sequence", "solve", "summary", "t")
@@ -58,7 +58,6 @@ is.name <- is.symbol
 
 assign("::", function(pkg, name) NULL, envir = .ArgsEnv)
 assign(":::", function(pkg, name) NULL, envir = .ArgsEnv)
-assign("%*%", function(x, y) NULL, envir = .ArgsEnv)
 assign("...length", function() NULL, envir = .ArgsEnv)
 assign("...names",  function() NULL, envir = .ArgsEnv)
 assign("...elt", function(n) NULL, envir = .ArgsEnv)
@@ -193,6 +192,13 @@ assign("untracemem", function(x) NULL, envir = .ArgsEnv)
     fx <- function(e1, e2) {}
     for(f in c("+", "-", "*", "/", "^", "%%", "%/%", "&", "|",
                "==", "!=", "<", "<=", ">=", ">")) {
+        body(fx) <- substitute(UseMethod(ff), list(ff=f))
+        environment(fx) <- .BaseNamespaceEnv
+        assign(f, fx, envir = env)
+    }
+
+    fx <- function(x, y) {} ## "matrixOps"
+    for(f in c("%*%")) {
         body(fx) <- substitute(UseMethod(ff), list(ff=f))
         environment(fx) <- .BaseNamespaceEnv
         assign(f, fx, envir = env)

src/library/base/man/crossprod.Rd
@@ -14,8 +14,7 @@
   \code{x \%*\% t(y)} (\code{tcrossprod}).
 }
 \usage{
-crossprod(x, y = NULL)
-
+ crossprod(x, y = NULL)
 tcrossprod(x, y = NULL)
 }
 \arguments{

src/library/base/man/groupGeneric.Rd
@@ -9,6 +9,7 @@
 \alias{.Group}
 \alias{Math}
 \alias{Math.data.frame}
+\alias{matrixOps}
 \alias{Ops}
 \alias{Ops.data.frame}
 \alias{Summary}
@@ -18,10 +19,10 @@
 \concept{group generic}
 \title{S3 Group Generic Functions}
 \description{
-  Group generic methods can be defined for four pre-specified groups of
-  functions, \code{Math}, \code{Ops}, \code{Summary} and \code{Complex}.
+  Group generic methods can be defined for the following pre-specified groups of
+  functions, \code{Math}, \code{Ops}, \code{matrixOps}, \code{Summary} and \code{Complex}.
   (There are no objects of these names in base \R, but there are in the
-  \pkg{methods} package.)
+  \pkg{methods} package, not yet for \code{matrixOps}.)
 
   A method defined for an individual member of the group takes
   precedence over a method defined for the group as a whole.
@@ -32,9 +33,10 @@
 \special{Ops(e1, e2)}
 \special{Complex(z)}
 \special{Summary(\dots, na.rm = FALSE)}
+\special{matrixOps(x, y)}
 }
 \arguments{
-  \item{x, z, e1, e2}{objects.}
+  \item{x, y, z, e1, e2}{objects.}
   \item{\dots}{further arguments passed to methods.}
   \item{na.rm}{logical: should missing values be removed?}
 }
@@ -42,8 +44,8 @@
 \details{
   %% --------------- grep -nw DispatchGroup src/*/*[ch]
 
-  There are four \emph{groups} for which S3 methods can be written,
-  namely the \code{"Math"}, \code{"Ops"}, \code{"Summary"} and
+  There are five \emph{groups} for which S3 methods can be written,
+  namely the \code{"Math"}, \code{"Ops"}, \code{"Summary"}, \code{"matrixOps"}, and
   \code{"Complex"} groups.  These are not \R objects in base \R, but
   methods can be supplied for them and base \R contains
   \code{\link{factor}}, \code{\link{data.frame}} and
@@ -130,6 +132,17 @@
 
     If the members of this group are called as functions, any argument
     names are removed to ensure that positional matching is always used.
+
+    \item Group \code{"matrixOps"}:
+    \itemize{
+      \item \code{"\%*\%"}
+      % do_matprod [array.c:1251]: if (DispatchGroup("matrixOps", ...))
+    }
+    This group currently contains the matrix multiply \code{\%*\%} binary operator
+    only, where at least \code{\link{crossprod}()} and \code{tcrossprod()}
+    are meant to follow.
+    Members of the group have the same dispatch semantics (using \emph{both} arguments)
+    as the \code{Ops} group.
 
     \item Group \code{"Summary"}:
     \itemize{

src/library/base/man/matmult.Rd
@@ -1,6 +1,6 @@
 % File src/library/base/man/matmult.Rd
 % Part of the R package, https://www.R-project.org
-% Copyright 1995-2017 R Core Team
+% Copyright 1995-2023 R Core Team
 % Distributed under GPL 2 or later
 
 \name{matmult}
@@ -26,9 +26,11 @@ x \%*\% y
   Promotion of a vector to a 1-row or 1-column matrix happens when one
   of the two choices allows \code{x} and \code{y} to get conformable
   dimensions.
-  
-  This operator is S4 generic but not S3 generic.  S4 methods need to be
-  written for a function of two arguments named \code{x} and \code{y}.
+
+  This operator is a generic function: methods can be written for it
+  individually or via the \code{\link[=S3groupGeneric]{matOps}} group
+  generic function; it dispatches to S3 and S4 methods.  Methods need to be
+  written for a function that takes two arguments named \code{x} and \code{y}.
 }
 \value{
   A double or complex matrix product.  Use \code{\link{drop}} to remove

src/library/tools/R/QC.R
@@ -217,7 +217,7 @@ function(package, dir, lib.loc = NULL)
         ## and are documented in base's groupGenerics.Rd.
         code_objs <- setdiff(code_objs,
                              c("Arith", "Compare", "Complex", "Logic",
-                               "Math", "Math2", "Ops", "Summary"))
+                               "Math", "Math2", "Ops", "Summary", "matrixOps"))
     }
 
     undoc_things <-
@@ -9220,7 +9220,7 @@ function(package_name)
     c("<-", "=",
       if(package_name == "base")
       c("(", "{", "function", "if", "for", "while", "repeat",
-        "Math", "Ops", "Summary", "Complex"),
+        "Math", "Ops", "Summary", "Complex", "matrixOps"),
       if(package_name == "utils") "?",
       if(package_name == "methods") "@")
 }

src/library/tools/R/utils.R
@@ -1238,7 +1238,7 @@ function(env, nms = NULL)
 
 .get_S3_group_generics <-
 function()
-    c("Ops", "Math", "Summary", "Complex")
+    c("Ops", "Math", "Summary", "Complex", "matrixOps")
 
 ### ** .get_S3_primitive_generics
 
@@ -1269,7 +1269,9 @@ function(include_group_generics = TRUE)
           ## Group 'Summary':
           "all", "any", "sum", "prod", "max", "min", "range",
           ## Group 'Complex':
-          "Arg", "Conj", "Im", "Mod", "Re")
+          "Arg", "Conj", "Im", "Mod", "Re",
+          ## Group 'matrixOps'
+          "%*%")
     else
         base::.S3PrimitiveGenerics
 }
@@ -1736,6 +1738,8 @@ function(parent = parent.frame())
     assign("Math", function(x, ...) UseMethod("Math"),
            envir = env)
     assign("Ops", function(e1, e2) UseMethod("Ops"),
+           envir = env)
+    assign("matrixOps", function(e1, e2) UseMethod("matrixOps"),
            envir = env)
     assign("Summary", function(..., na.rm = FALSE) UseMethod("Summary"),
            envir = env)

src/library/utils/R/objects.R
@@ -104,7 +104,7 @@ function(generic.function, class, envir=parent.frame())
 	    generic.function <- deparse1(substitute(generic.function))
         ## else
         if(!exists(generic.function, mode = "function", envir = envir) &&
-           !any(generic.function == c("Math", "Ops", "Complex", "Summary")))
+           !any(generic.function == c("Math", "Ops", "Complex", "Summary", "matrixOps")))
             stop(gettextf("no function '%s' is visible", generic.function),
                  domain = NA)
         warn.not.generic <- FALSE

commit 197fe9a8cf610f64c0ee597bf650899d074c383c

revert breakage from r84154
git-svn-id: https://svn.r-project.org/R/trunk@84157 00db46b3-68df-0310-9c12-caf00c1e9a41

src/library/base/R/namespace.R
@@ -830,6 +830,7 @@ requireNamespace <- function (package, ..., quietly = FALSE)
     ns <- .Internal(getRegisteredNamespace(package))
     res <- TRUE
     if (is.null(ns)) {
+        if(!quietly)
             packageStartupMessage(gettextf("Loading required namespace: %s",
                                            package), domain = NA)
         value <- tryCatch(loadNamespace(package, ...), error = function(e) e)